720fa3828cdf9ceb98b0ceb69b157386f8830456
Bmeza05 <brianmeza952@gmail.com>
2021-07-11 16:29:06 -0600

    inititial commit

---
+++ src/componets/first.jsx
@@ -0,0 +1,15 @@
+class App extends Component {    
+        render(){        
+            return (            
+                <>                
+                    <h1>Hello World</h1>                
+                    <p>Things to do:</p>
+                    <h1>Stop climate change</h1>
+                    <h1>become fully self sufficient</h1>
+                    <h1>Get a haircut</h1>
+                
+                </>        
+            );    
+        }
+    }
+    
